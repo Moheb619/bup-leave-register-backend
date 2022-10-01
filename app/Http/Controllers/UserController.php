06f18112cf92e128bac36d879528b5b24f5f18f4
Moheb619 <6moheb19@gmail.com>
2022-10-02 03:45:44 +0600

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    // Register
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'full_name' => 'required',
+            'contact' => 'required|unique:App\Models\User',
+            'email' => 'required|email|unique:App\Models\User',
+            'user_name' => 'required|unique:App\Models\User',
+            'password' => 'required',
+            'user_category' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('BUPLEAVEREGISTERBYMOHEB')->plainTextToken;
+        $success['full_name'] =  $user->name;
+
+        return response()->json([
+            "status" => "User Registered Successfully",
+            "message" => $success
+        ], 200);
+    }
+
+    // Login
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['user_name' => $request->user_name, 'password' => $request->password])) {
+            $user = Auth::user();
+            $success['full_name'] =  $user->name;
+            $success['token'] =  $user->createToken('BUPLEAVEREGISTERBYMOHEB')->plainTextToken;
+
+            return response()->json([
+                "status" => "User Logged In",
+                "message" => $success
+            ], 200);
+        } else {
+            return "Unauthorized";
+        }
+    }
+
+    public function all_user()
+    {
+        $users = User::all();
+        return response()->json([
+            "message" => "User Fetch Successful",
+            "user" => $users
+        ], 200);
+    }
+    public function delete_user($id)
+    {
+        $user = User::findOrFail($id);
+        if ($user) {
+            $user->delete();
+            return response()->json([
+                "message" => "User Deleted Successfully",
+                "user" => $user
+            ], 200);
+        } else {
+            return "Delete Failed";
+        }
+    }
+}
